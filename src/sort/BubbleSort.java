83c9537a6eff9fcf7c709b0ed67b12607c6070d9
BeHappyWsz <865744553@qq.com>
2018-02-28 21:13:58 +0800

    冒泡排序内部循环i控制手误，调整

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -22,7 +22,7 @@ public class BubbleSort {
 	public static void bubble(int[] arr) {
 		int temp;
 		for(int i = 1; i < arr.length; i++) {
-			for(int j = 0; j < arr.length-1; j++) {
+			for(int j = 0; j < arr.length-i; j++) {
 				if(arr[j] > arr[j+1]) {
 					temp     = arr[j];
 					arr[j]   = arr[j+1];
